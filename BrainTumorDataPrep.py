06838c67152e4f3a663d1520b9a57065c66f7eed
ogdenk <ogdenk@upstate.edu>
2018-06-20 16:28:08 -0400

    Working on project

--- BrainTumorDataPrep.py
+++ BrainTumorDataPrep.py
@@ -1,2 +1,12 @@
 # Program to autopopulate matrix of data spreadsheets and identify data with seperate key matrix
 
+import os
+
+# make sure that the Drobo is mounted and findable
+os.getcwd()
+print(os.path.exists("/Volumes/Public/PosteriorFossaTumors"))
+
+# display all folders in Posterior Fossa Tumors
+
+for root, name, dirs in os.walk("/Volumes/Public/PosteriorFossaTumors", topdown=True):
+    print(root, name)
