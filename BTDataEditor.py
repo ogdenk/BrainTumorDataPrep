41f0ace1aeafa3dbdb23ee7903ffa4ccf044eb9a
ogdenk <ogdenk@upstate.edu>
2018-06-20 17:06:03 -0400

    Working on project

--- BTDataEditor.py
+++ BTDataEditor.py
@@ -27,21 +27,22 @@ while continueYN == "y":
     locationflaire = "/Volumes/Public/PosteriorFossaTumors/PAT" + patientNum + "/flaire.tsv"
     locationDWIe = "/Volumes/Public/PosteriorFossaTumors/PAT" + patientNum + "/DWIe.tsv"
 
-    # import .tsv file as panda dataframe for manipulation
+    # import .tsv file as panda dataframe for manipulation, need to change .from_csv to .read_csv
     dfT1 = pd.DataFrame.from_csv(locationT1, sep='\t', header=0)
-    dfT2 = pd.DataFrame.from_csv(locationT2, sep='\t', header=0)
+    # dfT1 = pd.read_csv(locationT1)
+    # dfT2 = pd.DataFrame.from_csv(locationT2, sep='\t', header=0)
     # dfflair = pd.DataFrame.from_csv(locationflair, sep='\t', header=0)
     # dfDWI = pd.DataFrame.from_csv(locationDWI, sep='\t', header=0)
 
     # find all entries with Feature Class: info
     info_entriesT1 = dfT1[dfT1['Feature Class'] == 'info']
-    info_entriesT2 = dfT2[dfT2['Feature Class'] == 'info']
+    # info_entriesT2 = dfT2[dfT2['Feature Class'] == 'info']
     # info_entriesflair = dfflair[dfflair['Feature Class'] == 'info']
     # info_entriesDWI = dfDWI[dfDWI['Feature Class'] == 'info']
 
     # remove all entries with Feature Class: info
     editdfT1 = dfT1[dfT1['Feature Class'] != 'info']
-    editdfT2 = dfT2[dfT2['Feature Class'] != 'info']
+    # editdfT2 = dfT2[dfT2['Feature Class'] != 'info']
     # editdfflair = dfflair[dfflair['Feature Class'] != 'info']
     # editdfDWI = dfDWI[dfDWI['Feature Class'] != 'info']
 
@@ -63,6 +64,10 @@ while continueYN == "y":
     #editdfT1.drop(columns=['Image type', 'Feature Class'])
     del editdfT1['Image type']
     del editdfT1['Feature Class']
+
+    #replace NAN and INF with 0 in values column, use DataFrame.fillna()
+    editdfT1.replace([np.inf, -np.inf], np.nan)  # not working atm
+    editdfT1.fillna(0, inplace=True)
 
     # Save edited data frame to a new tsv file--ends up comma delineated, ok?
     editdfT1.to_csv(locationT1e)
