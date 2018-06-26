a0125a3ee2b073d2844266ddf48d7e9db4f54e00
ogdenk <ogdenk@upstate.edu>
2018-06-26 10:22:31 -0400

    Working on project

--- BTDataEditor.py
+++ BTDataEditor.py
@@ -28,11 +28,10 @@ while continueYN == "y":
     locationDWIe = "/Volumes/Public/PosteriorFossaTumors/PAT" + patientNum + "/DWIe.tsv"
 
     # import .tsv file as panda dataframe for manipulation, need to change .from_csv to .read_csv
-    dfT1 = pd.DataFrame.from_csv(locationT1, sep='\t', header=0)
-    # dfT1 = pd.read_csv(locationT1)
-    # dfT2 = pd.DataFrame.from_csv(locationT2, sep='\t', header=0)
-    # dfflair = pd.DataFrame.from_csv(locationflair, sep='\t', header=0)
-    # dfDWI = pd.DataFrame.from_csv(locationDWI, sep='\t', header=0)
+    dfT1 = pd.read_csv(locationT1, index_col = 0, parse_dates = True, sep = '\t', header = 0)
+   # dfT2 = pd.read_csv(locationT2, index_col = 0, parse_dates = True, sep = '\t', header = 0)
+   # dfflair = pd.read_csv(locationflair, index_col = 0, parse_dates = True, sep = '\t', header = 0)
+   # dfDWI = pd.read_csv(locationDWI, index_col = 0, parse_dates = True, sep = '\t', header = 0)
 
     # find all entries with Feature Class: info
     info_entriesT1 = dfT1[dfT1['Feature Class'] == 'info']
@@ -61,13 +60,12 @@ while continueYN == "y":
     editdfT1.insert(0, 'PatNum', PatNum)
 
     # remove Image Type and Feature Class columns
-    #editdfT1.drop(columns=['Image type', 'Feature Class'])
     del editdfT1['Image type']
     del editdfT1['Feature Class']
 
     #replace NAN and INF with 0 in values column, use DataFrame.fillna()
-    editdfT1.replace([np.inf, -np.inf], np.nan)  # not working atm
-    editdfT1.fillna(0, inplace=True)
+    editdfT1.replace('inf', 0, inplace = True)  # for some reason inf is not registering as a numpy expression
+    editdfT1.fillna(0, inplace = True)  # inplace = True overwrites the original file, same as df = df.fillna()
 
     # Save edited data frame to a new tsv file--ends up comma delineated, ok?
     editdfT1.to_csv(locationT1e)
