5d92c3429156b7290ef8f5079076f5a2a0d760bb
ogdenk <ogdenk@upstate.edu>
2018-06-27 12:01:47 -0400

    Working on project

--- BrainTumorDataPrep.py
+++ BrainTumorDataPrep.py
@@ -26,7 +26,7 @@ for root, dirs, files in os.walk(pathName, topdown = True):
     dirs[:] = [d for d in dirs if d.startswith('PAT')]  # only look in folders that start with PAT?
     listOfFiles += [os.path.join(root, file) for file in files]  # not really needed, redundant to be removed later
     listOfFNames += files  # create list of .tsv files from all PAT folders
-    listOfPAT += dirs  # incorrect, only gives one instance each instead of listing the folder name for each file within
+    listOfPAT += dirs  # incorrect, only gives one instance each instead of listing the folder name for each file
 
 # remove pathName and 'filename from root list
 j = 0
@@ -44,9 +44,9 @@ while i < listLength:
 fileMatrix = np.column_stack((listOfFiles, listOfFNames))
 # print(fileMatrix)
 
-k = 0
-
-dataSet = np.array([])
+col_num = 256 * len(listOfPAT) + 1
+total_pats = len(listOfPAT)
+dataSet = np.empty([3364, col_num - 1], dtype = object)  # rows, columns
 # dataSet[:, 0] = listOfFNames
 # dataSet[:, 0] = listOfAttributes
 # dataSet[] = tumorType
@@ -59,13 +59,30 @@ total_rows = df.shape[0]
 
 # not in while loop as we only want to do this once at the beginning
 info_entries = df['Feature Name'].tolist()
+attributes = np.array(info_entries)
+dataSet = np.insert(dataSet, 0, attributes, axis = 1)
 
-dataSet = np.append(dataSet, info_entries[:])  # currently doesn't work adds info_entries as a row, want column :(
-
+# create a list of patient names, there should be 256 entries of each name
+count = 0
+pat = 0
+i = 1
+patNum = np.empty([1, col_num], dtype = object)
+current = listOfPAT[0]
+patNum[0, 0] = ''
+while i < col_num:
+    patNum[0, i] = current
+    if count == 256:
+        count = 0
+        pat = pat + 1
+        if pat < total_pats:
+            current = listOfPAT[pat]
+    i = i + 1
+    count = count + 1
+dataSet = np.insert(dataSet, 0, patNum, axis = 0)
+k = 0
 while k < listLength:
     patientNum = fileMatrix[k, 0]
     fileName = fileMatrix[k, 1]
-    fileNameEdit = fileName + 'E'
 
     # generate locations for input and output files
     location = pathName + "/" + patientNum + "/" + fileName + ".tsv"
